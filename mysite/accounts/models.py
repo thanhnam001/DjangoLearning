5bbd6a6e08475b7043d7cc0ea9ab468f3c9e3c48
thanhnam001 <thanhnam040501@gmail.com>
2022-04-20 10:42:02 +0700

    video4 database models and admin panel

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -1,3 +1,39 @@
+import email
+from enum import auto
+from operator import mod
+from sre_constants import CATEGORY
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    phone = models.CharField(max_length=200,null=True)
+    email = models.EmailField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+    
+    def __str__(self):
+        return self.name
+
+class Products(models.Model):
+    CATEGORY =(
+        ('Indoor','Indoor'),
+        ('Outdoor','Outdoor'),
+    )
+
+    name = models.CharField(max_length=200,null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200,null=True,choices=CATEGORY)
+    description = models.CharField(max_length=200,null=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out of delivery','Out of delivery'),
+        ('Delivered','Delivered'),
+    )
+    # customer = 
+    # product =
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=200, null =True,choices=STATUS)
