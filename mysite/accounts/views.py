33322dd789a51f43dae52b43cf0414fe69c5a6b0
thanhnam001 <thanhnam040501@gmail.com>
2022-04-22 19:25:37 +0700

    video18+19 creating customer profiles with django signals

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -23,9 +23,9 @@ def registerPage(request):
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
-            group = Group.objects.get(name='customer')
-            user.groups.add(group)
-            Customer.objects.create(user=user,name=username)
+            # group = Group.objects.get(name='customer')
+            # user.groups.add(group)
+            # Customer.objects.create(user=user,name=username)
             messages.success(request, 'Account was created for '+username)
             return redirect('login')
     context = {'form':form}
