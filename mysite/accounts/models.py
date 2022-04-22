9a553b27a6471d787412b2dd9cde36be6c03b571
thanhnam001 <thanhnam040501@gmail.com>
2022-04-22 15:00:51 +0700

    video16 user profile with OneToOne relationship

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -6,9 +6,11 @@ from sre_constants import CATEGORY
 from turtle import ondrag
 from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200,null=True)
     phone = models.CharField(max_length=200,null=True)
     email = models.EmailField(null=True)
--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -24,6 +24,7 @@ def registerPage(request):
             username = form.cleaned_data.get('username')
             group = Group.objects.get(name='customer')
             user.groups.add(group)
+            Customer.objects.create(user=user,name=username)
             messages.success(request, 'Account was created for '+username)
             return redirect('login')
     context = {'form':form}
@@ -55,16 +56,22 @@ def home(request):
     customers = Customer.objects.all()
     
     total_customers = customers.count()
-    total_orders = orders.count()
 
+    total_orders = orders.count()
     delivered = orders.filter(status='Delivered').count()
     pending =  orders.filter(status='Pending').count()
     context = {'orders':orders,'customers':customers,'total_customers':total_customers,
         'total_orders':total_orders,'delivered':delivered,'pending':pending}
     return render(request,'accounts/dashboard.html',context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context={}
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending =  orders.filter(status='Pending').count()
+    context={'orders':orders,'total_orders':total_orders,'delivered':delivered,'pending':pending}
     return render(request,'accounts/user.html',context)
 
 @login_required(login_url='login')
